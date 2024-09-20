2abd185bbda71038eabb06fad04ffbb160a0b37a
Barbi Adkins <badkins0043@gmail.com>
2024-09-20 16:38:05 -0400

    added 2 truths and changes css

--- public/Barbara_adkins/script.js
+++ public/Barbara_adkins/script.js
@@ -1,11 +1,9 @@
-
 window.console.log("My name is Barbara Adkins.  I am from Hazard, Ky. ");
 
 const clickMeButtonElement = window.document.querySelector("#click-me-button");
 
 if (clickMeButtonElement) {
-    clickMeButtonElement.addEventListener("click", function() {
-        window.alert("You clicked me!");
-    
-});
+  clickMeButtonElement.addEventListener("click", function () {
+    window.alert("You clicked me!");
+  });
 }
